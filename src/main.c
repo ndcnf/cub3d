fedf6842d3258eeb0cd5411fc206689e9db5015b
Elwoll <zimapro@protonmail.com>
2022-11-18 13:12:10 +0100

    init struct

--- src/main.c
+++ src/main.c
@@ -12,11 +12,31 @@
 
 #include "../inc/cub3d.h"
 
+int init_struct(t_data *d)
+{
+	d = ft_calloc(1, sizeof(t_data));
+	if (alloc_check(d) != SUCCESS)
+		return (ERROR);
+	d->ray = ft_calloc(1, sizeof(t_rayunits));
+	if (alloc_check(d) != SUCCESS)
+		return (ERROR);
+	d->x = ft_calloc(1, sizeof(t_raycasting));
+	if (alloc_check(d) != SUCCESS)
+		return (ERROR);
+	d->y = ft_calloc(1, sizeof(t_raycasting));
+	if (alloc_check(d) != SUCCESS)
+		return (ERROR);
+	return (SUCCESS);
+}
+
+
 int main(int ac, char **av)
 {
 	t_data		data;
 
 	ft_bzero(&data, sizeof(t_data));
+	if (init_struct(&data) != SUCCESS)
+		return (0);
 	if (main_parsing(ac, av, &data) != SUCCESS)
 		return (0);
 	main_minimap(&data);
